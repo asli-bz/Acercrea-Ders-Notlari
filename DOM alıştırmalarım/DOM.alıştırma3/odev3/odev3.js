73ab701142e309676808f2917dbb2793cf87c7f3
asli boz <57016025+asli-boz@users.noreply.github.com>
2020-05-19 04:55:32 +0300

    .

--- DOM alıştırmalarım/DOM.alıştırma3/odev3/odev3.js
+++ DOM alıştırmalarım/DOM.alıştırma3/odev3/odev3.js
@@ -1,7 +1,7 @@
 function KontrolEt()
 {
     var mail = document.getElementById("mail").value;//id ile ınputun degerini çektik
-    var sorgu = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var sorgu = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     //regex ile aranıcak kelimeleri yaptık
 if(sorgu.test(mail) == false) //if ile harfler yazılana uymuyorsa yani false ise hata mesajıyla ife girer
     {//döngü ile de  yapılabilir
