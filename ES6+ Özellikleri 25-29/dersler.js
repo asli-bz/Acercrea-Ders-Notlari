92b7d22ebc8212188b0f22244ad7a4f3352ef825
asli boz <57016025+asli-boz@users.noreply.github.com>
2020-05-28 19:28:01 +0300

    .

--- ES6+ Özellikleri 25-29/dersler.js
+++ ES6+ Özellikleri 25-29/dersler.js
@@ -1,5 +1,6 @@
 /* ECRO SRİPT6+ özellikleri
-//Arrow function = ecro spript6 ile gelen fonksiyonları yazmamızı kısaltabilen söz dizimi
+-------------------------------------------------------------------------------------------------
+//ARROW FUNCTİON = ecro spript6 ile gelen fonksiyonları yazmamızı kısaltabilen söz dizimi
 arka planda yine fonksiyonlarımız oluşuyor arrow function ile daha kısa lşekilde yazılabilir
 
 const merhaba= function(){
@@ -27,4 +28,59 @@ console.log(cube(4));
 const cube = x => x * x * x;//tek işlem yapıp ve bu tek işlem return ise gerek yok
 console.log(cube(4));
  -----------------------------------------------------------------------------------------------*/
+
+ /*DESTRUCTİNG =  Destructuring assignment yine kod yazmayı çok pratikleştiren bir özelliktir. 
+//Hem dizilerle hem objelerle kullanılabiliyor. Dizi ve objeyi toplu bir şekilde değişkenlere atmamızı sağlıyor.
+//bu bizim eski yöntemimiz bunu destructing ile kısaca yapabiliriz
  
+//arraylerde ve dizilerde destructing
+let number1,number2;
+ arr=[100,200,300,400];
+ number1=arr[0];
+ number2=arr[1];
+ console.log(number1,number2);
+
+ //Destructuring ile 1.yol
+ let number1,number2;
+ arr=[100,200,300,400];
+
+ [number1,number2]=arr;//arraydeki 4 değerimizi buraya vermiş olduk
+// [number1,number2]=[100,200,300,400]; bu kısımda da number1 0. indexi number2 ise 1.indexi almış oluyr
+
+ console.log(number1,number2);
+//Destructuring ile 2.yol
+arr=[100,200,300,400];
+
+const [number1,number2]=arr;
+console.log(number1,number2);
+//değişkenlerimizi array ile oluşturucaksak Destructuring ile yapabiliriz
+
+//objelerde destructing
+const number ={
+    a:10,
+    b:20,
+    c:30,
+    d:40,
+    e:50
+};
+const {a,c,d} = number;//bunun sebebi ise isimlerini farklı vermemiz,objelere sıra sıra bakmıyor özelliklerine göre bakıyor
+console.log(a,c,d);//yaptığımızda değeri bize underfine veriyor 
+//aynı isim olarak verme3k istemiyorsak const{a:number1,c:number2}= number şeklinde 
+
+//functionlarda destructing
+ const getlangs = () =>["python","java","c++"];
+ const [lang1,lang2,lang3]= getlangs();
+ console.log(lang1,lang2,lang3); 
+ 
+//obje 2 
+const person={
+    name : "mustafa murat",
+    year : 1994,
+    salary : 3000,
+    showInfos : () => console.log("bilgiler...")
+}//objemizi oluşturduk
+const {name:isim,year:yil,salary:maas,showInfos:bilgilerigöster} = person;//objedekileri değişkene atarken {} kllanılır
+
+console.log(isim,yil,maas);
+bilgilerigöster();
+*/ 
