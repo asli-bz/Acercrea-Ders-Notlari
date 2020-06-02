193409590dbf56552239713798d526f213497f67
asli boz <57016025+asli-boz@users.noreply.github.com>
2020-06-02 03:54:59 +0300

    .

--- DOM alıştırmalarım/DOM.alıştırma4/3/ders.js
+++ DOM alıştırmalarım/DOM.alıştırma4/3/ders.js
@@ -4,25 +4,22 @@ let options = {
     height: 200
   };
   /*a)Bu objeyi destructing yöntemi ile değişkenlere aktarınız.
-  */
-  const {a:genis,b:widt,c:heigh} = options;
-  console.log(genis,widt,heigh);
-  
+   */
+  const {title:a,width:b,height:c} = options;
+  console.log(a,b,c);
+ 
 /*
   b)Bu objenin içerisinden sadece title’ı değişkene aktarınız.
    */
  const{title:ders}=options;
  console.log(ders);
-
-
+ 
  /*c)Bu objenin içerisinden aynı anda title’ı bir değişkene, width ve height’i ayrı bir objeye spread ile aktarınız. 
  */
-const yeni = {title:des,width:genis,height: yuksek} = options;
-const [des] = yeni;
-const [des,...degis2] = yeni;
-console.log(des);
-console.log(degis2);
-
+const yeni = {title,width,height} = options;
+const ornek={...options}
+console.log(title);
+console.log(ornek);
 
 
 
