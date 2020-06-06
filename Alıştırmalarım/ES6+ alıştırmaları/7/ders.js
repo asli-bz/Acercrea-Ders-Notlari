b33b4157db0ae2c50354034bc20b4174f840286e
asli boz <57016025+asli-boz@users.noreply.github.com>
2020-06-06 16:40:02 +0300

    .

--- Alıştırmalarım/ES6+ alıştırmaları/7/ders.js
+++ Alıştırmalarım/ES6+ alıştırmaları/7/ders.js
@@ -1,6 +1,18 @@
 const html = new Map();
-html.set("html",3);
-html.set("css",5);
-html.set("JS",10);
-html.set("nodeJs",10);
+html.set("id","html");
+html.set("price",3);
+html.set("id","css");
+html.set("price",5);
+html.set("id","Js");
+html.set("price",10);
+html.set("id","NodeJs");
+html.set("price",10);
 console.log(html);
+
+const htmlset = new Set();
+htmlset.add({id:"html",price:3});
+htmlset.add({id:"css",price:5});
+htmlset.add({id:"js",price:10});
+htmlset.add({id:"NodeJs",price:10});
+
+console.log(htmlset);
